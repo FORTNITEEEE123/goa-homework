d2882fc205a1eb5f18b2fee69073bd5672e51327
FORTNITEEEE123 <lukaotarashvili25@gmail.com>
2024-09-21 21:45:53 +0400

    first commit

---
+++ day01/1.lesson1.py
@@ -0,0 +1,29 @@
+name="my name is luka otarashvili"
+surname="my birthday is on january 30th"
+age="i am 11 old my heigh 157.2"
+heigh="now i am going to study in goa best coding school in georgia even is world"
+know_programing="i am in 6th class and i know georgian english russian and germany. now i have to do my homework in goa."
+knows_programin="my mothers and father names are diana and bejani"
+knos_programing="my sister and brother names are ana and nika"
+nows_programing="i love video game like fortnite fc mobile and more"
+knws_programing="i love sasung,poco,nokia and i hate iphone"
+knows_prgraming="now i am tring to learn codig in goa "
+knows_progming="i have four villige chramgesi, kldeisi, acana and lagodexi "
+knows_proraming="i love building legos pussels and more"
+knowsprograming="i love riding bikes and drones "
+kows_programing="gota go beacose i have to do my homework"
+
+print(name) 
+print(surname)
+print(age)
+print(heigh)
+print(know_programing)
+print(knows_programin)
+print(knos_programing)
+print(nows_programing)
+print(knws_programing)
+print(knows_prgraming)
+print(knows_progming)
+print(knows_proraming)
+print(knowsprograming)
+print(kows_programing)
---
+++ day01/lesson1.py
@@ -0,0 +1,31 @@
+# name="luka"
+# name არის ცვლადი
+# = არის ცვლადისთვის  მნინშვნელობის მიმნიჭებელი სიმბოლო
+# "luka" არის ცვლადისთვის მნიშვნელობა
+
+surname="otarashvili"
+
+# print(name)
+# პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიეცტი
+ 
+name="luka"  # ეს არის str (string) ტიპის ცვლადი
+age=11 # ეს არის int  (interger) მთელი რიცხვი
+heigh=157.2 # ეს არის float ტიპის ცვლადი (ათწილადი)
+# bollean (bool) ტიპის ცვლადი
+
+knows_programming=True #true or false
+is_ugly=False# snakecase (უბრალოდ წერუს სტილი სტანდარტულად)
+
+isUgly=False # ჯავასკრიპტული camelcase
+
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(heigh))
+#print(type(knows_programming))
+
+print(name +" "+ str (age))
