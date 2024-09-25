f30cd47b36832bea86958b0ad8eeddcd96ef0672
FORTNITEEEE123 <lukaotarashvili25@gmail.com>
2024-09-25 21:04:33 +0400

    second commit

--- DAY05/classwork/classwork.py
+++ DAY05/classwork/classwork.py
@@ -4,3 +4,10 @@ age=input("please enter your age ")
 about_your_self=input("please tell me about your self ")
 
 print("my name is "+name+" my surname is "+surname+" my age is "+age+" "+about_your_self)
+
+
+# name-->input()
+# surname=input(
+# age=input(lluka)
+# your_self="input()"
+# about_your=input("lukaaa)
