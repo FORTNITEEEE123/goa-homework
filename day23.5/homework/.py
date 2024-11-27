fc673923a559822d6e027343ca4fb5cc57897129
FORTNITEEEE123 <lukaotarashvili25@gmail.com>
2024-11-27 19:00:11 +0400

    second commit

---
+++ day23.5/homework/.py
@@ -0,0 +1,1 @@
+print("clouldnt make homework")
---
+++ day23/classwork/classwork.py
@@ -0,0 +1,20 @@
+# 1) გადააქციეთ string "hello" დიდ ასოებად .upper-ის გამოყენებით
+
+print("hello".upper())
+
+#2) გადააქციეთ string "GOA" პატარა ასოებად .lower-ის გამოყენებით
+
+print("GOA".lower())
+
+#3) გამოიყენეთ ფუნქცია .capitalize სტრინგზე: "georgia"
+
+print("georgia".capitalize())
+
+#4)string "hello" - ში მოძებნეთ ასობგერა "d" და დაპრინტეთ მისი ინდექსი
+
+# print("hello".find("d"))
+#გამოიტანს ერორს იმიტომრომ helloში არ არის d
+
+#5) string "GOA" - ში მოძებნეთ ასობგერა "A" და დაპრინტეთ მისი ინდექსი 
+
+print("GOA".find("A"))
