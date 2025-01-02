7dd849bdb1fe979fbe134b8aa5b4b01d22ffd619
FORTNITEEEE123 <lukaotarashvili25@gmail.com>
2025-01-02 20:53:31 +0400

    second commit

---
+++ day 32/homework/1).py
@@ -0,0 +1,4 @@
+# 1)ახსენით რა განსხვავებაა inline, internal, external css ტექნიკებს შორის.
+print(" Inline CSS გამოიყენება html-ში style ის დახმარებით")
+print("Internal CSS გამოიყენება html-ში head თეგში")
+print(" External CSS  იწერება ცალკე css ფაილში და უკავშირდება html-ს link-ის დახმარბით")
